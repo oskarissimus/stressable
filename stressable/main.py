ac74bfef3603ab5940cf47eadb4e00ceb9ab9772
oskarissimus <oskar.korczak@protonmail.com>
2023-01-03 14:43:02 +0100

    added endpoints for waiting

--- stressable/main.py
+++ stressable/main.py
@@ -1,8 +1,21 @@
+import random
 from fastapi import FastAPI
-
+from asyncio import sleep
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-    
+@app.get("/instant")
+async def instant():
+    return {"message": "ok"}
+
+@app.get("/wait")
+async def wait():
+    await sleep(2)
+    return {"message": "ok"}
+
+@app.get("/wait-random")
+async def wait_random():
+    time_to_wait = random.randint(1, 3)
+    await sleep(time_to_wait)
+    return {"message": "ok"}
+
+
