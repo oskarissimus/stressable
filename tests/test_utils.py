373ed5af9725abbdc85143210cc527419dc73dc7
Oskar Korczak <oskar.korczak@protonmail.com>
2023-04-18 11:28:31 +0200

    Added unit tests for stressable/utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,5 +3,8 @@ import stressable.utils
 def test_rot13():
     assert stressable.utils.rot13('hello') == 'uryyb'
 
-def test_rot13_empty_string():
+def test_empty_string():
     assert stressable.utils.rot13('') == ''
+
+def test_numbers():
+    assert stressable.utils.rot13('123') == '123'
