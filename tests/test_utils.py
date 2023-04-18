a42fbbe838161e76c87d18bae310f35134e7248c
Oskar Korczak <oskar.korczak@protonmail.com>
2023-04-18 13:17:57 +0200

    n/a

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,4 @@
-import codecs
-def test_rot13():
-    assert codecs.encode('test', 'rot13') == 'grfg'
+import unittest
+class TestRot13(unittest.TestCase):
+    def test_rot13_encodes_correctly(self):
+        self.assertEqual(rot13('test'), 'grfg')
