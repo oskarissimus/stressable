c4d04cdc480fc2446cd40d1ed12d14e72a2ebbff
Oskar Korczak <oskar.korczak@protonmail.com>
2023-04-18 11:34:09 +0200

    autogenerated_commit_message

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,6 @@
 import stressable.utils
 
+
 def test_rot13():
     assert stressable.utils.rot13('hello') == 'uryyb'
-
-def test_empty_string():
-    assert stressable.utils.rot13('') == ''
-
-def test_numbers():
-    assert stressable.utils.rot13('123') == '123'
+    assert stressable.utils.rot13('world') == 'jbeyq'
